b05694a17720842a2e1cf91fc0361c366b843c21
Elly Okello <ellyokelo@gmail.com>
2024-05-02 08:58:08 +0000

    Refactor hangman game logic and variable names in hangman.py

--- hangman.py
+++ hangman.py
@@ -1,24 +1,27 @@
 
+# Import the random module
 import random
 
-word_list = ["python", "java", "hangman", ...]  # Define the word_list variable
-attempts = 6
+# List of words for the game
+words = ["python", "java", "hangman", "javascript", "docker"]
 
-while attempts > 0:
-    # Game logic will go here
-    random_word = random.choice(word_list)
-    print("_" * len(random_word))
-  
+random_word = random.choice(words)
+
+# Now you can print underscores for each letter in the random word
+print("_" * len(random_word))
+
 guessed_letters = []  # Define the guessed_letters variable
 
 guess = input("Guess a letter: ")
 game_over = False
+attempts = 7  # Define the attempts variable
+
 while not game_over:
     # Game logic goes here
     if guess in random_word:
         guessed_letters.append(guess)
     else:
-        attempts -= 1
+        attempts -= 1  # Decrement attempts if the guess is wrong
 
     if set(random_word).issubset(set(guessed_letters)):
         print("Congratulations, you won!")
@@ -26,7 +29,3 @@ while not game_over:
     elif attempts == 0:
         print("Sorry, you lost. The word was:", random_word)
         game_over = True
-
-    if game_over:
-        break
-
